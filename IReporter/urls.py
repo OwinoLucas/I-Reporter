55677a994754129c19d98285e21c45c50505cc5e
victormongare1 <vicmongz254@gmail.com>
2020-06-18 15:28:44 +0300

    created view class and url pattern for getting profiles

--- IReporter/urls.py
+++ IReporter/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
+from IReporter.views import ProfileList 
 
 urlpatterns=[
-    
+    url(r'^api/profiles$',ProfileList.as_view()),
 ]
--- IReporter/views.py
+++ IReporter/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-
+from IReporter.models import Profile
+from IReporter.serializers import ProfileSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 # Create your views here.
+class ProfileList(APIView):
+    '''
+    class to define view for the profile api endpoint
+    '''
+    def get(self, request, format=None):
+        all_profiles=Profile.objects.all()
+        serializers=ProfileSerializer(all_profiles,many=True)
+        return Response(serializers.data)
+    def post():
+
+
