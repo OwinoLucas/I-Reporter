0544cf88cdae41742ab3e736f331557acf634c92
victormongare1 <vicmongz254@gmail.com>
2020-06-18 21:54:08 +0300

    fixed bugs in code

--- IReporter/tests.py
+++ IReporter/tests.py
@@ -2,8 +2,4 @@ from django.test import TestCase
 from .models import Profile
 
 # Create your tests here.
-class ProfileTestClass(Testcase):
-    '''
-    test class to test methods and object instances of the Profile class
-    '''
-    def setUp(self):
+
