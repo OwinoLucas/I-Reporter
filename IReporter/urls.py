fdc7ab24d47cff42ce0fa536426b9acca7328ac8
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-06-18 14:29:17 +0300

    changed path to url to sync with rest of code

--- IReporter/urls.py
+++ IReporter/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.urls import path,re_path 
 from .views import intervention_list,intervention_detail,intervention_list_status
 urlpatterns=[
-    path(r'api/interventionrecords',intervention_list.as_view()),
-    re_path(r'^api/interventionrecords/(?P<pk>[0-9]+)$',intervention_detail.as_view()),
-    path(r'api/interventionrecords/resolved' ,intervention_list_status.as_view())
+    url(r'^api/interventionrecords/$',intervention_list.as_view()),
+    url(r'^api/interventionrecords/(?P<pk>[0-9]+)$',intervention_detail.as_view()),
+    url(r'api/interventionrecords/resolved/$' ,intervention_list_status.as_view())
 ]
