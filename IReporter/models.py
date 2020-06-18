7b4c779f3dafa29950dd94d042c5d45ec70d5fdf
victormongare1 <vicmongz254@gmail.com>
2020-06-18 12:47:56 +0300

    created fields for th fields for the profile model

--- IReporter/models.py
+++ IReporter/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
+class Profile(models.Model):
+    '''
+    profile class to define profile objects
+    '''
+    profile_picture=CloudinaryField('picture',blank=True)
+    bio=models.CharField(max_length=100,blank=True)
+    contacts=models.CharField(max_length=30,blank=True)
+
+
