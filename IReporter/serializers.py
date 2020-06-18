e217855b6404f9c2d2758efa086dc870d3abf776
victormongare1 <vicmongz254@gmail.com>
2020-06-18 13:53:07 +0300

    created serializer class

--- IReporter/serializers.py
+++ IReporter/serializers.py
@@ -1,1 +1,8 @@
-# from rest_framework import serializers 
+from rest_framework import serializers
+from IReporter.models import Profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model=Profile
+        fields=('id',"profile_picture",'bio','contacts')
