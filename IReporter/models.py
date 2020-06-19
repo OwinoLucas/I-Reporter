ae653ef2b670357446cd3dba38f03a3747809a9a
moselamar <44118179+moselamar@users.noreply.github.com>
2020-06-19 08:10:00 +0300

    create flag model

--- IReporter/models.py
+++ IReporter/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
+class Flag(models.Model):
+    name = models.CharField(max_length=60)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, default='')
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, default='')
+    tags=models.ManyToManyField()
