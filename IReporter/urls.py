8066467bb44c80ca8a68c60887119bf7dd521f80
Kvria <trishakuria@gmail.com>
2020-06-23 05:22:43 +0300

    add CreateUserAPIView

--- IReporter/urls.py
+++ IReporter/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
-from .views import CreateUserAPIView,LoginApiView,intervention_list,intervention_detail,intervention_list_status
-from django.urls import path,re_path 
+from .views import intervention_list,intervention_detail,intervention_list_status,CreateUserAPIView
+# from django.urls import path 
 
 app_name='IReporter'
 urlpatterns=[
     url(r'^signup/$', CreateUserAPIView.as_view()),
-    url(r'^login/$', LoginApiView.as_view()),
+    # url(r'^login/$', LoginApiView.as_view()),
     url(r'^api/interventionrecords/$',intervention_list.as_view()),
     url(r'^api/interventionrecords/(?P<pk>[0-9]+)$',intervention_detail.as_view()),
     url(r'api/interventionrecords/resolved/$' ,intervention_list_status.as_view()), 
