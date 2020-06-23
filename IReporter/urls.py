cd6fdbdb61f5201954684b9200d88ecf4702a6aa
mosesbabu <44118179+mosesbabu@users.noreply.github.com>
2020-06-23 10:10:28 +0300

    modify flag urls

--- IReporter/urls.py
+++ IReporter/urls.py
@@ -18,9 +18,11 @@ urlpatterns=[
     url(r'^intervention-records-status/(?P<intervention_status>[A-Za-z]+)/$' ,InterventionListStatus.as_view(), name='filter-by-status'),
     url(r'^api/profiles$',ProfileList.as_view()),
     url(r'^api/profile/(?P<pk>[0-9]+)$',SingleProfile.as_view()),
-    url(r'^api/flags/$',flag_list.as_view()),
-    url(r'^api/flags/(?P<pk>[0-9]+)$',flag_detail.as_view())
-
+    url(r'^api/flags/$',AllFlagRecords.as_view(),name='allflags'),
+    url(r'^api/flags/(?P<pk>[0-9]+)$',FlagDetail.as_view()),
+    url(r'^api/flags/create/$',CreateFlag.as_view()),
+    url(r'^api/flags/status(?P<flag_status>[A-Za-z]+)/$' ,FlagStatus.as_view(), name='filter-by-status'),
+    url(r'^Flags/(?P<title>[A-Za-z]+)/$',FlagList.as_view(), name='flag list records fetch'),
     url(r'^signup/$', CreateUserAPIView.as_view()),
     url(r'^login/$', LoginApiView.as_view()),
 ]
