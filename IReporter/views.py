395c817cfcd010e9fbc3832d7b7572810f32d348
Kvria <trishakuria@gmail.com>
2020-06-23 05:24:35 +0300

    add CreateUserAPIView class based view

--- IReporter/views.py
+++ IReporter/views.py
@@ -2,68 +2,73 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework_jwt.settings import api_settings
-from .serializers import UserSerializer,InterventionSerializer,UserRegSerializer
-from .models import User,InterventionRecord
-import jwt
+from .serializers import InterventionSerializer,UserSerializer
+from .models import InterventionRecord 
 from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser 
 from rest_framework.decorators import api_view,APIView,permission_classes
-
-
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
-from rest_framework import status
-from .serializers import UserSerializer
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 
 class CreateUserAPIView(APIView):
     # Allow any user (authenticated or not) to access this url 
-    permission_classes = (AllowAny,) 
+    # permission_classes = (AllowAny,) 
     def post(self, request):
-        # Validating our serializer from the UserRegSerializer
-        serializer = UserRegSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        # Everything's valid, so send it to the UserSerializer
-        model_serializer = UserSerializer(data=serializer.data)
-        model_serializer.is_valid(raise_exception=True)
-        model_serializer.save()
-        return Response(model_serializer.data, status=status.HTTP_201_CREATED)
-        
-class LoginApiView(APIView):
-    permission_classes = (AllowAny,)
-    def post(self, request):
-        try:
-            email = request.data['email']
-            password = request.data['password']
-
-            user = User.objects.get(email=email, password=password)
+        # Validating our serializer from the UserSerializer
+        user_serializer = UserSerializer(data=request.data)
+        if user_serializer.is_valid():
+            user = user_serializer.save()
+            subject = 'welcome'
+            body = '''Greetings from the I-reporter Team,
+                    Hello''' + user_serializer.data['username'] + ''', we are glad having you as one of our
+                    entrusted clients to give news and update the different agencies on the country's development.
+                    
+                    We ensure that your voices will be heard!
+                    Cheers, 
+                    The I-reporter team. '''
+            sender = settings.EMAIL_HOST_USER
+            receiver = user_serializer.data['email']
             
-            # import pdb; pdb.set_trace()
-            if user:
-                try:
-                    jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-                    jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
-                    payload = jwt_payload_handler(user)
-                    token = jwt_encode_handler(payload)
-                    user_details = {}
-                    user_details['name'] = "%s %s" % (
-                        user.first_name, user.last_name)
-                    user_details['token'] = token
-                    # user_logged_in.send(sender=user.__class__,
-                    #                     request=request, user=user)               
-                    return Response({'msg': 'Login successful', 'user_details':user_details }, status=status.HTTP_200_OK)
+            send_mail(subject,body,sender,[receiver])
+            return Response({'detail' : 'User successfully created.'}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(user_serializer.errors,status = status.HTTP_400_BAD_REQUEST)
+        
+# class LoginApiView(APIView):
+#     permission_classes = (AllowAny,)
+#     def post(self, request):
+#         try:
+#             email = request.data['email']
+#             password = request.data['password']
 
-                except:
-                    return Response({'msg': 'Account not approved or wrong Password.'}, status=status.HTTP_409_CONFLICT)
-            else:           
-                return Response({'msg': 'Can not authenticate with the given credentials or the account has been deactivated'}, status=status.HTTP_403_FORBIDDEN)
-        except KeyError:
-            return Response({'msg': 'please provide a email and a password'}, status=status.HTTP_401_UNAUTHORIZED)
+#             user = User.objects.get(email=email, password=password)
+            
+#             # import pdb; pdb.set_trace()
+#             if user:
+#                 try:
+#                     jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+#                     jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+#                     payload = jwt_payload_handler(user)
+#                     token = jwt_encode_handler(payload)
+#                     user_details = {}
+#                     user_details['name'] = "%s %s" % (
+#                         user.first_name, user.last_name)
+#                     user_details['token'] = token
+#                     # user_logged_in.send(sender=user.__class__,
+#                     #                     request=request, user=user)               
+#                     return Response({'msg': 'Login successful', 'user_details':user_details }, status=status.HTTP_200_OK)
+
+#                 except:
+#                     return Response({'msg': 'Account not approved or wrong Password.'}, status=status.HTTP_409_CONFLICT)
+#             else:           
+#                 return Response({'msg': 'Can not authenticate with the given credentials or the account has been deactivated'}, status=status.HTTP_403_FORBIDDEN)
+#         except KeyError:
+#             return Response({'msg': 'please provide a email and a password'}, status=status.HTTP_401_UNAUTHORIZED)
 
 class intervention_list(APIView):
     def get(self,request):
